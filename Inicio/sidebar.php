f208cff68c8e555f0487d3fa016508e76a52abce
LHanccoC <lhanccoc@unjbg.edu.pe>
2025-07-15 11:11:13 -0500

    Cambio style1 y sidebar

--- Inicio/sidebar.php
+++ Inicio/sidebar.php
@@ -2,12 +2,12 @@
     <a href="../inicio/index.php">Inicio</a>
     <p>Unidad I</p>
     <a href="../MetodoM/dashMetodM.php">Método de la gran M</a>
-    <a href="#">Método Simplex</a>
+    <a href="MetodoSimplex/metodoSimplex.php">Método Simplex</a>
     <a href="#">Método Dual</a>
     <p>Unidad II</p>
     <a href="../ModeloTransporte/dashTransporte.php">Modelo de Transporte</a>
     <a href="#">Ramificación y acotamiento</a>
-    <a href="#">Binario</a>
+    <a href="../ModeloBinario/modeloBinario.php">Binario</a>
     <a href="#">Asignación</a>
 </nav>
 <main>
