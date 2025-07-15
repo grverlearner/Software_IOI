b0040e3f2374cd8d91f44eaeb7f1167b96a86375
LHanccoC <lhanccoc@unjbg.edu.pe>
2025-07-15 11:39:46 -0500

    Correccion link MetodoSimplex

--- Inicio/sidebar.php
+++ Inicio/sidebar.php
@@ -2,7 +2,7 @@
     <a href="../inicio/index.php">Inicio</a>
     <p>Unidad I</p>
     <a href="../MetodoM/dashMetodM.php">Método de la gran M</a>
-    <a href="MetodoSimplex/metodoSimplex.php">Método Simplex</a>
+    <a href="../MetodoSimplex/metodoSimplex.php">Método Simplex</a>
     <a href="../DUAL/dual_form.php">Método Dual</a>
     <p>Unidad II</p>
     <a href="../ModeloTransporte/dashTransporte.php">Modelo de Transporte</a>
